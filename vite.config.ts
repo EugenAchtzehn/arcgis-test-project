824e93af78cbf7a12a43c41238e6e07888d79957
Eugene <eugene@omniguider.com>
2025-08-20 17:54:03 +0800

    [*] try to externalize @arcgis/core but not succeed. Also, rollup analyzer added.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from "node:url";
+import path from "node:path";
 
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -8,11 +9,28 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
+// analyze file size of each module
+import { visualizer } from "rollup-plugin-visualizer";
+
+// 嘗試外部化 @arcgis/core，不參與打包過程，尚未成功
+import { viteStaticCopy } from "vite-plugin-static-copy";
+// alternative plugin: "vite-plugin-html";
+// currently not support vite 7
+import { insertHtml, h } from "vite-plugin-insert-html";
+
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
     base: env.VITE_BASE_URL,
+    // optimizeDeps: {
+    //   exclude: ["@arcgis/core"],
+    // },
+    // build: {
+    //   rollupOptions: {
+    //     plugins: [],
+    //   },
+    // },
     plugins: [
       vue({
         template: {
@@ -29,6 +47,29 @@ export default defineConfig(({ mode }) => {
       Components({
         resolvers: [ElementPlusResolver()],
       }),
+      visualizer({
+        // automatically open the report after build
+        open: true,
+        // report's path will be <root>/stats.html
+        filename: "stats.html",
+      }),
+      // viteStaticCopy({
+      //   targets: [
+      //     {
+      //       src: "node_modules/@arcgis/core/",
+      //       dest: "vendor/esri",
+      //     },
+      //   ],
+      // }),
+      // insertHtml({
+      //   head: [
+      //     h("link", {
+      //       rel: "stylesheet",
+      //       href: `${env.VITE_BASE_URL}vendor/esri/core/assets/esri/themes/light/main.css`,
+      //     }),
+      //     h("script", { src: `${env.VITE_BASE_URL}vendor/esri/core/Map.js`, type: "module" }),
+      //   ],
+      // }),
     ],
     css: {
       preprocessorOptions: {
